9e6c38904c90cd271f3159d64d3b37ab5bb06b3b
Jacek <astanoskillos@gmail.com>
2012-12-22 12:46:13 +0100

    aa

--- Konfigurator/Konfigurator/MainWindow.xaml.cs
+++ Konfigurator/Konfigurator/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace Konfigurator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
