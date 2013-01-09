dbf858ff75a902457d3d90998f5655afe17e3be3
Jacek <astanoskillos@gmail.com>
2013-01-09 15:53:49 +0100

    dane klienta

---
+++ Konfigurator/Konfigurator/dane_klienta.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Konfigurator
+{
+    /// <summary>
+    /// Interaction logic for dane_klienta.xaml
+    /// </summary>
+    public partial class dane_klienta : Page
+    {
+        public dane_klienta()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
