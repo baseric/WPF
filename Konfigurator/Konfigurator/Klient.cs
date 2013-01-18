aa38ac8de31834c316fd3cd5dab7a3b88421ce92
taksun <taksun8@o2.pl>
2013-01-18 11:06:01 +0100

    dodany poczatek bazy

---
+++ Konfigurator/Konfigurator/Klient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Konfigurator
+{
+    class Klient
+    {
+        String name;
+        String adres;
+        String kod;
+        String msc;
+        String nip;
+
+        public Klient(String _name, String _adres, String _kod, String _msc, String _nip)
+        {
+            name = _name;
+            adres = _adres;
+            kod = _kod;
+            msc = _msc;
+            nip = _nip;
+        }
+    }
+}
---
+++ Konfigurator/Konfigurator/MyDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Konfigurator
+{
+    class MyDB
+    {
+        private static Collection<Zamowienie> list = new ObservableCollection<Zamowienie>();
+
+        static MyDB()
+        {
+            list.Add(new Zamowienie("Jan", "Kowalski", "", ""));
+            list.Add(new Zamowienie("Roman", "Kowalski", "", ""));
+            list.Add(new Zamowienie("Zdzisław", "Kowalski", "", ""));
+            list.Add(new Zamowienie("Wiesław", "Kowalski", "", ""));
+        }
+
+        private static MyDB singleton = null;
+
+        public static MyDB Instance
+        {
+            get
+            {
+                if (singleton == null)
+                    singleton = new MyDB();
+                return singleton;
+            }
+        }
+    }
+}
