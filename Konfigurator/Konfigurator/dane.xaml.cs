9e78ca9c9aa6868848918c3320eeb0b4cd73d8e1
Jacek <astanoskillos@gmail.com>
2013-01-23 15:42:14 +0100

    okienko dane

---
+++ Konfigurator/Konfigurator/dane.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Konfigurator
+{
+    /// <summary>
+    /// Interaction logic for dane.xaml
+    /// </summary>
+    public partial class dane : Window
+    {
+        public dane()
+        {
+            InitializeComponent();
+        }
+    }
+}
